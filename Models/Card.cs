91a6a734556d7c01ef79128b2641a50c527d3093
Nikodem <npotulski@gmail.com>
2023-12-28 21:58:03 +0100

    added Deck class with Shuffle and DealHand methods

--- Models/Card.cs
+++ Models/Card.cs
@@ -2,7 +2,7 @@
 {
     public enum Suit
     {
-        Hearts,
+        Hearts, // 0
         Diamonds,
         Clubs,
         Spades
@@ -10,19 +10,19 @@
 
     public enum Rank
     {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King,
-        Ace
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six= 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 14
     }
 
     public class Card
--- Models/Deck.cs
+++ Models/Deck.cs
@@ -1,6 +1,44 @@
-﻿namespace HoldemOddsAPI.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace HoldemOddsAPI.Models
 {
     public class Deck
     {
+        private List<Card> cards;
+
+        public Deck()
+        {
+            cards = Enumerable
+                .Range(0,4) // Suits
+                .SelectMany(s=> Enumerable
+                    .Range(2,13) //Ranks
+                    .Select(r=> new Card((Suit)s, (Rank)r)))
+                .ToList();
+            
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            var rnd = new Random();
+            int n = cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                (cards[k], cards[n]) = (cards[n], cards[k]);
+            }
+        }
+
+        public Hand DealHand()
+        {
+            if(cards.Count <2) throw new InvalidOperationException("Not enough cards to deal a hand.");
+            var hand = new Hand(cards[0], cards[1]);
+            cards.RemoveRange(0,2); //Remove the dealt cards from the deck
+            return hand;
+        }
     }
 }
