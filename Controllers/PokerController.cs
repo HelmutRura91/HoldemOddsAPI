22d105e5e0aa1eea2b03c2c748f9232e2b468e21
Nikodem <npotulski@gmail.com>
2023-12-31 18:33:58 +0100

    added PokerTable, PokerTableService and start-and-deal endpoint

--- Controllers/PokerController.cs
+++ Controllers/PokerController.cs
@@ -11,10 +11,77 @@ namespace HoldemOddsAPI.Controllers
     [ApiController]
     public class PokerController : ControllerBase
     {
-        private readonly DeckService _deckService;
-        public PokerController(DeckService deckService)
+        //injecting PokerTableService into PokerController constructor
+        private readonly PokerTableService _pokerTableService;
+        public PokerController(PokerTableService pokerTableService)
         {
-            _deckService = deckService;
+            _pokerTableService = pokerTableService;
+        }
+
+
+        [HttpGet("start-and-deal/{numberOfPlayers}")]
+        public IActionResult StartGameAndDealHands(int numberOfPlayers)
+        {
+            try
+            {
+                // Start the new game
+                _pokerTableService.StartNewGame(numberOfPlayers);
+
+                // Deal initial hands
+                var playerHands = _pokerTableService.DealInitialHands();
+                var handsInfo = playerHands.Select(ph => new
+                {
+                    Player = ph.Key.Id,
+                    Hand = ph.Value.ToString()
+                });
+
+                return Ok(new
+                {
+                    Message = $"Game started with {numberOfPlayers} players and initial hands dealt.",
+                    Hands = handsInfo
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("start-game/{numberOfPlayers}")]
+        public IActionResult StartGame(int numberOfPlayers)
+        {
+            try
+            {
+                _pokerTableService.StartNewGame(numberOfPlayers);
+                return Ok($"Game started with {numberOfPlayers} players.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("deal-initial-hands")]
+        public IActionResult DealInitialHands()
+        {
+            if (!_pokerTableService.IsGameStarted)
+            {
+                return BadRequest("Game has not bedd started.");
+            }
+            try
+            {
+                var playerHands = _pokerTableService.DealInitialHands();
+                var handsInfo = playerHands.Select(ph => new
+                {
+                    Player = ph.Key.Id,
+                    Hands = ph.Value.ToString()
+                });
+                return Ok(new { Message = "Initial hands dealt to all players.", Hands = handsInfo});
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("test-hand/{suit1}/{rank1}/{suit2}/{rank2}")]
@@ -31,33 +98,6 @@ namespace HoldemOddsAPI.Controllers
             {
                 return BadRequest(ex.Message);
             }
-        }
-
-        [HttpGet("shuffle")]
-        public IActionResult ShuffleDeck()
-        {
-            var deck = new Deck();
-            _deckService.InitializeDeck(deck);
-            deck.Shuffle();
-            var cardStrings = _deckService.GetAllCards(deck).Select(card=>card.ToString()).ToList();
-            return Ok(cardStrings);
-        }
-
-        [HttpGet("deal")]
-        public IActionResult DealHand()
-        {
-            try
-            {
-                var deck = new Deck();
-                _deckService.InitializeDeck(deck);
-                deck.Shuffle();
-                var hand = _deckService.DealHand(deck);
-                return Ok(hand.ToString());
-            }
-            catch(Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }            
         }
 
     }
---
+++ Services/PokerTableService.cs
@@ -0,0 +1,88 @@
+﻿using HoldemOddsAPI.Extensions;
+using HoldemOddsAPI.Models;
+
+namespace HoldemOddsAPI.Services
+{
+    public class PokerTableService
+    {
+        //private means that state of _pokerTable can only be manipulated through the methods provided by the service
+        //readonly means that _pokerTable variable can only be assigned a value during its declaration or in the constructor of the class
+        private readonly PokerTable _pokerTable;
+        private readonly DeckService _deckService;
+        
+        //changed the private field (_IsGameStarted) to a public property with a private setter, so PokerController could read the value
+        public bool IsGameStarted { get; private set; }
+
+        public PokerTableService(DeckService deckService)
+        {
+            _pokerTable = new PokerTable();
+            _deckService = deckService;
+        }
+
+        public void StartNewGame(int numberOfPlayers)
+        {
+            if (numberOfPlayers < 2 || numberOfPlayers > 9)
+            {
+                throw new ArgumentException("Number of players must be between 2 and 9.");
+            }
+
+            _pokerTable.Deck = new Deck();
+            _deckService.InitializeDeck(_pokerTable.Deck);
+            _pokerTable.Deck.Shuffle();
+            _pokerTable.Players.Clear();
+            _pokerTable.CommunityCards.Clear();
+            _pokerTable.Pot = 0;
+
+            for(int i= 0; i < numberOfPlayers; i++)
+            {
+                _pokerTable.Players.Add(new Player());
+            }
+
+            IsGameStarted = true;
+
+        }
+
+        public Dictionary<Player, Hand> DealInitialHands()
+        {
+            if (!IsGameStarted)
+            {
+                throw new InvalidOperationException("The game has not been started.");
+            }
+
+            var playerHands = new Dictionary<Player, Hand>();
+            foreach (var player in _pokerTable.Players)
+            {
+                var hand = _deckService.DealHand(_pokerTable.Deck);
+                player.SetHand(hand);
+                playerHands.Add(player, hand);
+            }
+            return playerHands;
+        }
+            
+
+        public void PlayFlop()
+        {
+            //deal the flop after the first round of betting
+        }
+
+        public void PlayTurn()
+        {
+
+        }
+
+        public void PlayRiver()
+        {
+
+        }
+
+        //public void HandlePlayerAction(Player player, PlayerAction action)
+        //{
+        //    // handle player actions (bet, fold, raise)
+        //}
+
+        //public Player DetermineWinner()
+        //{
+
+        //}
+    }
+}
